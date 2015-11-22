1329349fb37f9688a20a21d5f5d197d1b3eaecf6
franky <54314720@qq.com>
2015-11-22 23:07:45 +0800

    v1.1.0：修复sqlserver初始化脚本的系列bug

--- src/main/java/org/jeecgframework/web/system/controller/core/RepairController.java
+++ src/main/java/org/jeecgframework/web/system/controller/core/RepairController.java
@@ -56,6 +56,7 @@ public class RepairController extends BaseController {
 	@RequestMapping(params = "repair")
 	public ModelAndView repair() {
 		repairService.deleteAndRepair();
+//		repairService.repair();
 		systemService.initAllTypeGroups();   //初始化缓存
 		return new ModelAndView("login/login");
 	}
---
+++ src/main/java/org/jeecgframework/web/utils/StringUtil.java
@@ -0,0 +1,85 @@
+package org.jeecgframework.web.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by admin on 2015/11/21.
+ */
+public class StringUtil extends org.jeecgframework.core.util.StringUtil {
+    /**
+     * 处理value中的特殊字符
+     */
+    public static String trimSpecialText(String str) {
+        StringBuffer sb = new StringBuffer();
+        for (int i=0; i<str.length(); i++) {
+            char c = str.charAt(i);
+            switch (c){
+                case '\"':
+                    sb.append("\\\"");
+                    break;
+                case '\\':
+                    sb.append("\\\\");
+                    break;
+                case '/':
+                    sb.append("\\/");
+                    break;
+                case '\b':
+                    sb.append("\\b");
+                    break;
+                case '\f':
+                    sb.append("\\f");
+                    break;
+                case '\n':
+                    sb.append("\\n");
+                    break;
+                case '\r':
+                    sb.append("\\r");
+                    break;
+                case '\t':
+                    sb.append("\\t");
+                    break;
+                default:
+                    sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+
+    public static String getTextFromHtml(String htmlStr) {
+        if (StringUtil.isNotEmpty(htmlStr)){
+            String regEx_script = "<script[^>]*?>[\\s\\S]*?<\\/script>"; // 定义script的正则表达式
+            String regEx_style = "<style[^>]*?>[\\s\\S]*?<\\/style>"; // 定义style的正则表达式
+            String regEx_html = "<[^>]+>"; // 定义HTML标签的正则表达式
+            String regEx_space = "\\s*|\t|\r|\n";//定义空格回车换行符
+            Pattern p_script = Pattern.compile(regEx_script, Pattern.CASE_INSENSITIVE);
+            Matcher m_script = p_script.matcher(htmlStr);
+            htmlStr = m_script.replaceAll(""); // 过滤script标签
+
+            Pattern p_style = Pattern.compile(regEx_style, Pattern.CASE_INSENSITIVE);
+            Matcher m_style = p_style.matcher(htmlStr);
+            htmlStr = m_style.replaceAll(""); // 过滤style标签
+
+            Pattern p_html = Pattern.compile(regEx_html, Pattern.CASE_INSENSITIVE);
+            Matcher m_html = p_html.matcher(htmlStr);
+            htmlStr = m_html.replaceAll(""); // 过滤html标签
+
+            Pattern p_space = Pattern.compile(regEx_space, Pattern.CASE_INSENSITIVE);
+            Matcher m_space = p_space.matcher(htmlStr);
+            htmlStr = m_space.replaceAll(""); // 过滤空格回车标签
+            htmlStr = htmlStr.trim(); // 返回文本字符串
+//            htmlStr = htmlStr.replaceAll("&nbsp;", "");
+//            htmlStr = htmlStr.substring(0, htmlStr.indexOf("。") + 1);
+        }
+        return htmlStr;
+    }
+    public static String getSomechar(String text , int  length) {
+        String str="";
+        if(StringUtil.isNotEmpty(text)){
+             str=text.substring(0,text.length()>length?length:text.length());
+        }
+        return  str;
+    }
+
+}
