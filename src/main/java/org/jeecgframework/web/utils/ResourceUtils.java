6b9eb21131ad467a9510fcfde3c5af92df4ecb4f
franky <54314720@qq.com>
2016-02-01 11:34:23 +0800

    bate1:修改附件上传时候命名方式

--- src/main/java/org/jeecgframework/web/utils/ResourceUtils.java
+++ src/main/java/org/jeecgframework/web/utils/ResourceUtils.java
@@ -23,6 +23,20 @@ public class ResourceUtils extends FileUtils{
    }
 
     /***
+     　　　   获取文件名
+     @param fileName
+      * @return
+     **/
+    public static String getFileName(String fileName) {
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        String realFilename=sdf.format(new Date())+StringUtils.random(8);
+        String extend = ResourceUtils.getExtend(fileName, "");// 获取文件扩展名
+        if(org.apache.commons.lang.StringUtils.isEmpty(extend)){
+            return  realFilename;
+        }
+        return realFilename+"."+extend;//自定义文件名称
+    }
+    /***
      * 获取资源服务器的路径
      *
      * @return
